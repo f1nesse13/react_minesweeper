ce801b2876f1c3f0eca22cd5b19286d49bd92c25
f1nesse13 <jpell3130@gmail.com>
2019-07-06 00:29:44 -0400

    added board setup - renders rows and creates tiles within each which are passed various props for gamestate

---
+++ components/board.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Tile from './tile';
+
+export default class Board extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { board, updateGame } = this.props;
+    const gameBoard = board.grid.map((row, i) => {
+      return (
+        <div data-row={i} key={i}>
+          {row.map((tile, j) => {
+            return <Tile key={j} tile={tile} updateGame={updateGame} />;
+          })}
+        </div>
+      );
+    });
+    return <div>{gameBoard}</div>;
+  }
+}
